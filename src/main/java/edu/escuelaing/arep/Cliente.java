75c674a45fd9e364bb28bfa34abbaf923bbf6f89
David Andres Herrera <damoya2000@gmail.com>
2021-02-19 14:58:54 -0500

    final

--- src/main/java/edu/escuelaing/arep/Cliente.java
+++ src/main/java/edu/escuelaing/arep/Cliente.java
@@ -11,17 +11,32 @@ public class Cliente {
 
     public static void main(String[] args) {
         pruebaCalculadora();
+        pruebaFacade();
     }
 
+
     private static void pruebaCalculadora() {
-        HttpResponse<String> response = null;
+        HttpResponse<String> consulta = null;
         try {
-            response = Unirest.get("https://trigcalulator.herokuapp.com/Calcular?operacion=sin&dato=3.1416").asString();
+            consulta = Unirest.get("https://trigcalulator.herokuapp.com/Calcular?operacion=sin&dato=3.1416").asString();
         } catch (UnirestException e) {
             e.printStackTrace();
         }
         System.out.println("el resultado es: ");
-        System.out.println(response.getBody());
+        System.out.println(consulta.getBody());
+    }
+
+
+    private static void pruebaFacade() {
+
+        HttpResponse<String> consulta = null;
+        try {
+            consulta = Unirest.get("https://servicefacade.herokuapp.com/Operaciones?operacion=cos&dato=2").asString();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+        System.out.println("el resultado es: ");
+        System.out.println(consulta.getBody());
     }
 
 }
